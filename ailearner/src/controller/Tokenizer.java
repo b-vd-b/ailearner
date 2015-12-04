86fc1abc7009c7b7016ec8995dec5436e02e7342
b-vd-b <bram.guardian@gmail.com>
2015-12-04 15:39:05 +0100

    t

--- ailearner/src/controller/Tokenizer.java
+++ ailearner/src/controller/Tokenizer.java
@@ -2,4 +2,8 @@ package controller;
 
 public class Tokenizer {
 
+	public static String[] tokenize(String in){
+		String[] out = in.toLowerCase().replaceAll("[^a-z'0-9]"," ").split("\\s+");
+		return out;
+	}
 }
