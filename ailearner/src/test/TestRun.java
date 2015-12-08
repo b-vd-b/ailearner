be86ea5238e4474d2a4420f00f0b0f51f43db7c7
Yuri van Midden <mail@yurivanmidden.nl>
2015-12-08 17:39:17 +0100

    Het works better

--- ailearner/src/test/TestRun.java
+++ ailearner/src/test/TestRun.java
@@ -9,18 +9,18 @@ public class TestRun implements Definitions {
 	
 	public static void main(String[] args){
 		BagOfWords bow1 = new BagOfWords();
-		bow1.readAllWordsInCategory("F", bow1);
+		bow1.readAllWordsInCategory(CATEGORY_A, bow1);
 		//System.out.println(bow1.bagOfWords.toString());
 		BagOfWords bow2 = new BagOfWords();
-		bow2.readAllWordsInCategory("M", bow2);
+		bow2.readAllWordsInCategory(CATEGORY_B, bow2);
 		//System.out.println(bow2.bagOfWords.toString());
 		ProbabilityFormula.totalDistinctWords = ProbabilityFormula.countDistinctWords(bow1, bow2);
-		System.out.println("FemaleTotalWordCount:    " + bow1.totalWordCount);
-		System.out.println("MaleTotalWordCount:      " + bow2.totalWordCount);
-		System.out.println("FemaleDistinctWordCount: " + bow1.distinctWordCount);
-		System.out.println("MaleDistinctWordCount:   " + bow2.distinctWordCount);
-		System.out.println("TotalDistinctWordCount:  " + ProbabilityFormula.totalDistinctWords);
-		System.out.println("Whuhahahaha: " + Classifier.classify(Classifier.getTestFiles(CATEGORY_A), bow1, bow2));
+		System.out.println(CATEGORY_A+"WordCount: \t\t\t" + bow1.totalWordCount);
+		System.out.println(CATEGORY_B+"WordCount: \t\t\t" + bow2.totalWordCount);
+		System.out.println(CATEGORY_A+"WordCount: \t\t\t" + bow1.distinctWordCount);
+		System.out.println(CATEGORY_B+"WordCount: \t\t\t" + bow2.distinctWordCount);
+		System.out.println("TotalDistinctWordCount: \t" + ProbabilityFormula.totalDistinctWords);
+		System.out.println("Whuhahahaha: " + Classifier.classify(Classifier.getRandomTestFile(CATEGORY_A), bow1, bow2));
 	}
 
 }
